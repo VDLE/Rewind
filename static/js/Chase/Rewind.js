9dd3985439380a0814d7b088e326e81acfb74b7b
Vinh Le <vdacle@gmail.com>
2016-04-20 19:21:31 -0700

    New Piano Roll

--- static/js/Chase/Rewind.js
+++ static/js/Chase/Rewind.js
@@ -15,11 +15,65 @@ var pausePlayStop = function(stop) {
 		on = 0;
 	}
 };
-var unique = 0;
 var current = "static/music/naruto.mid";
 var path = "static/music/"
 var music;
+var roll;
+var length;
+var pixels = 2;
+var dtimes = {};
+function setup(){
+				// Set up roll
+				roll = $("#roll");
+				music = player.data;
+				length = player.endTime * 2 + "px";
+				roll.css("left", "220px");
+				roll.css("width", length);
+			
+				
+				var time = 0;
+				var notemap = new Array();
+				for (var n = 0; n < music.length; n ++) 
+				{
+					var event = music[n][0].event;
+					music[n][0].id = n;
+					time = time + music[n][1];
+					switch(event.subtype) {
 
+						case 'noteOn':
+							notemap["" + event.channel + " event " + event.noteNumber] = {time: time / 1000.0, id: n};
+							break;
+
+						case 'noteOff':
+							if ( ("" + event.channel + " event " + event.noteNumber) in notemap && notemap["" + event.channel + " event " + event.noteNumber] != null)
+							{
+								// love the name
+								notemapnote = notemap["" + event.channel + " event " + event.noteNumber];
+								var dtime = time/1000.0 - notemapnote.time;
+								notemap["" + event.channel + " event " + event.noteNumber] = null;
+								
+								// Start time and width or dtime
+								var starttime = notemapnote.time;
+								dtimes[notemapnote.id] = dtime;
+								
+								// Piano Roll Creation
+								var noteId = event.noteNumber - 21;
+								var offset = $('#'+noteId).offset();
+								var $div = $("<div>", {class: "note", id: notemapnote.id+"-note"});
+								$div.css("position", "absolute");
+								$div.css("top", offset.top);
+								$div.css("background-color", "#000");
+								$div.css("width", dtime*1000+"px" );
+								$div.css("left", starttime*1000+"px");
+								roll.append($div);
+							}
+							break;
+					}
+						
+					
+
+				}
+}
 eventjs.add(window, "load", function(event) {
 	var link = document.createElement("link");
 	link.href = "//fonts.googleapis.com/css?family=Oswald";
@@ -35,10 +89,9 @@ eventjs.add(window, "load", function(event) {
 	/// load up the piano keys
 	var colors = document.getElementById("colors");
 	var colorElements = [];
-	var dtimes = {};
 	for (var n = 87; n >= 0; n--) {
 		var d = document.createElement("div");
-		d.id = n;
+		d.id = 87 - n;
 		d.innerHTML = MIDI.noteToKey[n + 21];
 		colorElements.push(d);
 		colors.appendChild(d);
@@ -58,33 +111,7 @@ eventjs.add(window, "load", function(event) {
 			player.BPM = false;
 			player.loadFile(current, function(){
 
-				// Set up roll
-				music = player.data;
-				var time = 0;
-				var notemap = new Array();
-				for (var n = 0; n < music.length; n ++) 
-				{
-					var event = music[n][0].event;
-					music[n][0].id = n;
-					time = time + music[n][1];
-					switch(event.subtype) {
-
-						case 'noteOn':
-							notemap["" + event.channel + " event " + event.noteNumber] = {time: time / 1000.0, id: n};
-							break;
-
-						case 'noteOff':
-							if ( ("" + event.channel + " event " + event.noteNumber) in notemap)
-							{
-								// love the name
-								notemapnote = notemap["" + event.channel + " event " + event.noteNumber];
-								var dtime = time/1000.0 - notemapnote.time;
-								dtimes[notemapnote.id] = dtime;
-							}
-							break;
-					}
-
-				}
+				setup();
 
 			});
 
@@ -98,46 +125,21 @@ eventjs.add(window, "load", function(event) {
 
 				// Create Piano Key Div
 				var map = colorMap[data.note - 27];
-				var offset = $('#'+d.id).offset();
-				var $div = $("<div>", {class: "note"});
-				$div.css("top", offset.top);
-				$div.css("left", "100%");
-				$div.css("background-color", map.hex);
-				//var left = $div.offset().left;
-
 
 				if (d) {
 					if (data.message === 144) {
-						//$("#pausePlayStop").addClass("pulse");
 						if (map) d.style.background = map.hex;
 						d.style.color = "#fff";
-
-						// Piano Roll
-						$("#color2").append($div);
-						var width = $div.css("width").slice(0,-2);
-
-						var dtime = dtimes[data.id];
-						$div.css("width", $(window).width() * dtime / player.end * 10 + "px" ) ;//width * dtimes.shift() + "px");
-						$div.css("left", ($(window).width() * dtime / player.end * 10 + 35 + $(window).width() ) + "px");
-						$div.animate({
-							left:"0%", 
-							opacity:"show",
-						},  ($(window).width() + ($(window).width() * dtime / player.end * 10 - 35) ) / .5,"linear")
-						.promise().done(function(){
-							$div.remove();
-						});
-					} else {
-
-						// Bounce
-						//var elm = document.getElementById("pausePlayStop");
-						//var newone = elm.cloneNode(true);
-						//elm.parentNode.replaceChild(newone, elm);
 						
-						// Reset Color
+						// Piano Roll
+						$("#"+data.id+"-note").css("background-color", map.hex);
+						// var divwidth = $("#"+data.id+"-note").css("width");
+						// $("#roll").animate({left: -divwidth});
+						
+
+					} else {
 						d.style.background = "";
 						d.style.color = "";
-						
-
 					}
 				}
 			});
@@ -149,6 +151,7 @@ eventjs.add(window, "load", function(event) {
 });
 
 var toggle = 0;
+var check = 0;
 var MIDIPlayerPercentage = function(player) {
 	// update the timestamp
 	var time1 = document.getElementById("time1");
@@ -180,7 +183,11 @@ var MIDIPlayerPercentage = function(player) {
 		var now = data.now >> 0; // where we are now
 		var end = data.end >> 0; // end of song
 		var events = data.events;
-
+		if(check != now){
+			$("#roll").animate({left: -now*1000+"px"}, 800);
+		}
+		check = now;
+		
 		// display the information to the user
 		timeCursor.style.width = (percent * 100) + "%";
 		time1.innerHTML = timeFormatting(now);
@@ -218,6 +225,7 @@ function uploadAudioFile(file){
 $(document).ready(function() {
 	var icon = $('.play');
 	var unplayed = 0;
+	$( "#roll" ).draggable({ axis: "x" });
 	
 	icon.click(function() {
 		if(unplayed == 0){
@@ -234,13 +242,18 @@ $(document).ready(function() {
 			$(".main-btn").toggleClass('play', 10).toggleClass('pause', 10);
 		}
 		
-		
+		check = 0;
+		$("#roll").empty();
 		var temp = $(this).val().replace(/C:\\fakepath\\/i, '');
 		console.log(temp);
 		$("#file-text").val(temp);
 		//console.log($(this)[0].files[0]);
 		//uploadAudioFile($(this)[0].files[0]);
-		player.loadFile(path+temp);
+		player.loadFile(path+temp,function(){
+
+			setup();
+
+		});
 	});
 	
 //	$('.file-button').on('change', function() {
